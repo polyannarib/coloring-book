05cda6891161edcb088b35f808c4afc90d5f1308
Polyanna Ribeiro <polyanna5252@gmail.com>
2022-03-22 21:03:06 -0300

    Initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>
+                <span> C </span>
+                <span> O </span>
+            </h1>
+        </div>
+    )
+}
