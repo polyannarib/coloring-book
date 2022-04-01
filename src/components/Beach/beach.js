debede0562728e075c54a651d92079d1474f0274
Polyanna Ribeiro <polyanna5252@gmail.com>
2022-03-31 23:11:16 -0300

    beach drawing

--- src/components/Beach/beach.js
+++ src/components/Beach/beach.js
@@ -1,7 +1,26 @@
+import styles from "./beach.module.scss"
+
 export default function Beach() {
     return (
-        <div>
-            <div></div>
+        <div className={styles.container}>
+            <div className={styles.circle}>
+                <div className={styles.sandBottom} />
+                <div className={styles.sandTop} />
+                <div className={styles.sea} />
+                <div className={styles.sun} />
+                <div className={styles.clouds} style={{bottom: "80%", right: 50}}>
+                    <div className={styles.cloudHelper}/>
+                    <div />
+                    <div />
+                    <div />
+                </div>
+                <div className={styles.clouds} style={{bottom: "60%", left: 80}}>
+                    <div className={styles.cloudHelper} />
+                    <div />
+                    <div />
+                    <div />
+                </div>
+            </div>
         </div>
     )
 }
