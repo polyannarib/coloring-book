b2efdf7454731267c4258a3d60d9a0e731f0bb0c
Polyanna Ribeiro <polyanna5252@gmail.com>
2022-04-05 20:21:20 -0300

    flower coloring

--- src/components/Flower/flower.js
+++ src/components/Flower/flower.js
@@ -1,46 +1,92 @@
 import styles from "./flower.module.scss"
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useColor } from "react-color-palette";
 import Color from "../Color/color";
 
 export default function Flower() {
-    const [color, setColor] = useState("#F7E7CE");
+    const [color, setColor] = useColor("hex", "#F7E7CE");
+    const ids = ["petal-top-1", "petal-top-2", "petal-top-3", "petal-mid-1", "petal-mid-2",
+                 "petal-bottom-1", "petal-bottom-2", "petal-bottom-3", "circle"];
 
     useEffect(() => {
-        document.body.style.backgroundColor = color;
-    });
+        document.body.style.backgroundColor = color.hex;
+    }, []);
 
-    function setBgColor() {
-        setColor("#000")
+    function backgroundColor() {
+        document.body.style.backgroundColor = color.hex;
     }
+
+    function coloring(id) {
+        if(ids.includes(id)) {
+            document.getElementById(id).style.backgroundColor = color.hex
+        }
+    }
+
     return (
         <div className={styles.allContainer}>
-            <Color />
+            <div className={styles.colorContainer}>
+                <Color select={setColor} coloring={color} />
+                <button
+                    onClick={() => backgroundColor()}
+                    className={styles.bgButton}
+                >
+                    Change background color
+                </button>
+            </div>
             <div className={styles.flowerContainer}>
                 <div className={styles.flower}>
-                    <div className={styles.circle}></div>
+                    <div
+                        onClick={(e) => coloring(e.target.id)}
+                        id="circle"
+                        className={styles.circle}
+                    />
                     <div className={styles.petalsTop}>
-                        <div style={{top: 55, left: 55}}>
-                        </div>
-                        <div style={{top: 35}}>
-                        </div>
-                        <div style={{top: 55, right: 55}}>
-                        </div>
+                        <div
+                            onClick={(e) => coloring(e.target.id)}
+                            id="petal-top-1"
+                            style={{top: 55, left: 55}}
+                        />
+                        <div
+                            onClick={(e) => coloring(e.target.id)}
+                            id="petal-top-2"
+                            style={{top: 35}}
+                        />
+                        <div
+                            onClick={(e) => coloring(e.target.id)}
+                            id="petal-top-3"
+                            style={{top: 55, right: 55}}
+                        />
                     </div>
 
                     <div className={styles.petalsMid}>
-                        <div style={{right: 35}}>
-                        </div>
-                        <div style={{left: 35}}>
-                        </div>
+                        <div
+                            onClick={(e) => coloring(e.target.id)}
+                            id="petal-mid-1"
+                            style={{right: 35}}
+                        />
+                        <div
+                            onClick={(e) => coloring(e.target.id)}
+                            id="petal-mid-2"
+                            style={{left: 35}}
+                        />
                     </div>
 
                     <div className={styles.petalsBottom}>
-                        <div style={{bottom: 55, left: 55}}>
-                        </div>
-                        <div style={{bottom: 35}}>
-                        </div>
-                        <div style={{right: 55, bottom: 55}}>
-                        </div>
+                        <div
+                            onClick={(e) => coloring(e.target.id)}
+                            id="petal-bottom-1"
+                            style={{bottom: 55, left: 55}}
+                        />
+                        <div
+                            onClick={(e) => coloring(e.target.id)}
+                            id="petal-bottom-2"
+                            style={{bottom: 35}}
+                        />
+                        <div
+                            onClick={(e) => coloring(e.target.id)}
+                            id="petal-bottom-3"
+                            style={{right: 55, bottom: 55}}
+                        />
                     </div>
                 </div>
             </div>
